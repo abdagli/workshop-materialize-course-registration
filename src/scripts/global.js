a33228807e2925a105d5011eef8edca9cc87bb18
Abdullah Dağlı <abdagli@gmail.com>
2017-10-03 15:01:12 +0300

    ...

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,2 +1,8 @@
 $('.muhittin').sideNav();
-$('.uzm-course-payment .card').matchHeight();
+$('.uzm-payment .card').matchHeight();
+$('.uzm-signin .card').matchHeight();
+
+$('input[type=tel]').inputmask('+99 999 999 99 99');
+$('#cardnumber').inputmask('9999 9999 9999 9999');
+$('#cardMonth').inputmask('99');
+$('#cardYear').inputmask('9999');
